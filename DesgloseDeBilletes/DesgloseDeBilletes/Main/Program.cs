2e27ab9f98a83d0da8ef05ac21523cc69c5468b2
alexus21003 <us21003@ues.edu.sv>
2022-05-02 23:41:23 -0600

    Activity: 'Desglose de billetes' on console mode, finished.

---
+++ DesgloseDeBilletes/DesgloseDeBilletes/Main/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DesgloseDeBilletes.Logic;
+
+namespace DesgloseDeBilletes {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int cantidad = 0;
+
+            Console.Write("Digite una cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+            ContarBilletes c = new ContarBilletes(cantidad);
+
+            c.Calcular();
+            c.BilletesContados();
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
